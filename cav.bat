2c8d94f922b3a3594967bec6d01660e90c064139
Zly <zlyudchik@gmail.com>
2022-01-13 19:18:39 +0300

    Enchanced cav.bat, folder specification is optional now. Also, it looks more fancy.

--- cav.bat
+++ cav.bat
@@ -1,17 +1,22 @@
 @echo off
 
-IF "%1"=="" (
-	echo Specify an output folder...
-	pause > nul
-	exit
+SET OUT_DIR=%1
+
+IF "%OUT_DIR%"=="" (
+	echo [33mFile is not specified, using default name for it...[0m
+	SET OUT_DIR=output
+	TIMEOUT /T 3 >nul 2>nul
 )
 
-mkdir %1
+mkdir %OUT_DIR%
 
 FOR %%A IN (*) DO (
 	IF "%%~xA"==".mp4" (
-		ffmpeg -y -i "%%A" "%%~dpA\%1\%%~nA"_converted.mp4
+		ffmpeg -y -i "%%A" "%%~dpA\%OUT_DIR%\%%~nA"_converted.mp4
 	)
 )
 
+echo.
+echo [42mConverting is complete![0m
+echo.
 pause
