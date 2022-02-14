ee4d01d2e5f180f36a7c8970e78e5567aef7a279
Zly <zlyudchik@gmail.com>
2022-02-14 18:54:46 +0300

    Added gif converter, syntax: `gif.bat file_input [loops]`

---
+++ gif.bat
@@ -0,0 +1,12 @@
+@echo off
+echo syntax: gif.bat file_input loops
+
+SET LOOPS=%2
+
+IF "%LOOPS%"=="" (
+	SET LOOPS=0
+	echo Loops are set to infinite...
+	TIMEOUT /T 3 >nul 2>nul
+)
+
+ffmpeg -y -i "%1" -loop %LOOPS% "%~dpn1"_converted.gif
