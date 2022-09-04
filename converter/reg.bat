5241e601a6d8ec6d763c095bb8530fe6a4f74f29
Zly <zlyudchik@gmail.com>
2022-09-04 15:00:07 +0300

    deleted test regs, that is planned for later when i have strength learning how the hell registry scripting works lol.

--- converter/reg.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-reg add "HKEY_CLASSES_ROOT\*\shell\ffmpeg_MP3\command" /d "%~dp0converter_mp3_run.bat "%%V""
-:: reg add "HKEY_CLASSES_ROOT\*\shell\ffmpeg_MP4\command" /d "%~dp0convert_mp4.bat "%%V""
-:: reg add "HKEY_CLASSES_ROOT\*\shell\ffmpeg_WAV\command" /d "%~dp0convert_wav.bat "%%V""
-pause
